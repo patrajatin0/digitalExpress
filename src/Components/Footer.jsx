733526dc38f504d62a379b73179cafffa66a8e63
Jatin <patrajatin0@gmail.com>
2025-12-25 18:28:32 +0530

    small change in footer resposive

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,31 +1,35 @@
 const Footer = () => {
     return (
         <>
-            <div className=" bg-sky-400 justify-center items-center gap-20 p-30">
-                <div >
-                    <div className="flex items-center bg-orange-500 p-2 rounded-lg text-sky-50 cursor-pointer my-4 ">
-                        <i class="fa-brands fa-instagram"></i>
-                        <span>Instagram</span>
-                    </div>
-                    <div className="flex items-center bg-orange-500 p-2 rounded-lg text-sky-50 cursor-pointer my-4 ">
-                        <i class="fa-brands fa-facebook"></i>
-                        <span>Facebook</span>
-                    </div>
+
+            <div className="flex flex-col bg-sky-400 justify-center items-center gap-2 p-20">
+                <div className="border-b-2 border-b-gray-300 p-4   ">
+                    <h1 className="text-4xl text-gray-300  ">Digital Express</h1>
                 </div>
                 <div>
-                    <div className="flex items-center flex-col  p-2 rounded-lg  cursor-pointer my-4 text-orange-800 ">
+                    <div className="flex items-center flex-col  p-2 rounded-lg  cursor-pointer my-4 text-white ">
                         <i class="fa-solid fa-location-dot"></i>
                         <p>Odisha,Aska,Ganjam</p>
                         <i class="fa-solid fa-envelope"></i>
                         <p>digitalexpress@gmail.com</p>
                     </div>
                 </div>
+
+
                 <div>
-                    <div className="flex items-center bg-orange-500 p-2 rounded-lg text-sky-50 cursor-pointer my-4 ">
+                    <div className="flex items-center bg-orange-500 p-2 rounded-lg text-sky-50 cursor-pointer my-4 gap w-36 ">
+                        <i class="fa-brands fa-instagram"></i>
+                        <span>Instagram</span>
+                    </div>
+                    <div className="flex items-center bg-orange-500 p-2 rounded-lg text-sky-50 cursor-pointer my-4 gap w-36 ">
+                        <i class="fa-brands fa-facebook"></i>
+                        <span>Facebook</span>
+                    </div>
+                    <div className="flex items-center bg-orange-500 p-2 rounded-lg text-sky-50 cursor-pointer my-4 gap w-36 ">
                         <i class="fa-brands fa-whatsapp"></i>
                         <span>Whatsapp</span>
                     </div>
-                    <div className="flex items-center bg-orange-500 p-2 rounded-lg text-sky-50 cursor-pointer my-4 ">
+                    <div className="flex items-center bg-orange-500 p-2 rounded-lg text-sky-50 cursor-pointer my-4 gap w-36 ">
                         <i class="fa-solid fa-phone"></i>
                         <span>Phone Call</span>
                     </div>
